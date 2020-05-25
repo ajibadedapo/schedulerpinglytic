27e963786001f36100468e2c5805dcc2962ebe48
ajibadedapo <ajibadehammed@gmail.com>
2020-05-25 23:58:23 +0100

    first commit

--- script.py
+++ script.py
@@ -1,45 +1,16 @@
-import tweepy, requests
-from bs4 import BeautifulSoup
+import requests
 
-consumer_key = "0ychK8sOKKBMF07kDGK8f7hhM"
-consumer_secret = "bWuCj0TyraJySFx8NLJ2WUV1hr0slK0OblZ1iJMZzDSmpLPYgI"
-access_key = "859114956138192896-jYQO9zeobtLoSdVrqVbjPyFhwjxUEhC"
-access_secret = "Sfu3Dm2M9JYZSBQuY9Rz7kP2aMsp468KgQXzTr2OZP4Av"
+url = "https://uptimebackend.herokuapp.com/api/monitor/v1"
 
+payload = "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"files\"; filename=\"20180215-033512.mp4\"\r\nContent-Type: video/mp4\r\n\r\n\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"ip_address\"\r\n\r\n10.10.10.110\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"date\"\r\n\r\n2018-02-14 18:27:20\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--"
+headers = {
+    'content-type': "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW",
+    'Accept': "application/json",
+    'Content-Type': "application/x-www-form-urlencoded",
+    'Cache-Control': "no-cache",
+    'Postman-Token': "50de5d50-e0f9-d2ab-6b37-aee8aa127bf3"
+    }
 
-def share_tweet():
-    #tweetcount = input('How many tweets do you want to post ? ')
-    try:
-        #tweetcount = int(tweetcount)
-        tweetcount = 1
-    except:
-         print("This is not an integer !")
-         exit
-    tweetcountup = 0
+response = requests.request("POST", url, data=payload, headers=headers)
 
-    website = "https://islamvibe.herokuapp.com"
-    website = website.strip()
-    res = requests.get(website)
-    result = res.content
-    soup = BeautifulSoup(result, 'html.parser')
-    first_link = soup.find('h3').text
-    tweet = first_link
-    #for a in first_link:
-    print(tweet)
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_key, access_secret)
-    api = tweepy.API(auth)
-    print('logged into twitter')
-    try:
-        api.update_status(tweet)
-    except tweepy.error.TweepError:
-        api.update_status(tweet)
-    print('Tweets posted!')
-
-
-
-def main():
-    share_tweet()
-
-if __name__ == '__main__':
-    main()
+print(response.text)
